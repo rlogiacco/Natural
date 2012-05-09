c52d6b5b321c3eeac1327289ee56bf93c16770ce
Roberto Lo Giacco <rlogiacco@gmail.com>
2012-05-09 22:17:11 +0100

    added syntax highlighting

--- org.rlogiacco.eclipse.cucumber.ui/src/org/rlogiacco/eclipse/cucumber/ui/CucumberDSLUiModule.java
+++ org.rlogiacco.eclipse.cucumber.ui/src/org/rlogiacco/eclipse/cucumber/ui/CucumberDSLUiModule.java
@@ -5,6 +5,8 @@ package org.rlogiacco.eclipse.cucumber.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.xtext.ui.editor.hyperlinking.IHyperlinkHelper;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -18,4 +20,17 @@ public class CucumberDSLUiModule extends org.rlogiacco.eclipse.cucumber.ui.Abstr
 	public Class<? extends IHyperlinkHelper> bindIHyperlinkHelper() {
 		return CucumberDSLHyperlinkHelper.class;
 	}
+	
+	public Class<? extends IHighlightingConfiguration> bindIHighlightingConfiguration () {
+	    return CucumberDSLHighlightingConfiguration.class;
+	}
+	
+	public Class<? extends ISemanticHighlightingCalculator> bindISemanticHighlightingCalculator(){
+		 return CucumberDSLSemanticHighlightingCalculator.class;
+	}
+	
+//	@Override
+//	public Class<? extends org.eclipse.xtext.builder.IXtextBuilderParticipant> bindIXtextBuilderParticipant() {
+//		return org.eclipse.xtext.builder.BuilderParticipant.class;
+//	}
 }
