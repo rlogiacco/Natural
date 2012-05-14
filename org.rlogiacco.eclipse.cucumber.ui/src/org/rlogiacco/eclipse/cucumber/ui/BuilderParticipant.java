b9c49405c0fb4446e9635438fcad4dabe4624621
Roberto Lo Giacco <rlogiacco@gmail.com>
2012-05-14 11:47:48 +0100

    added fake builder participant

---
+++ org.rlogiacco.eclipse.cucumber.ui/src/org/rlogiacco/eclipse/cucumber/ui/BuilderParticipant.java
@@ -0,0 +1,10 @@
+package org.rlogiacco.eclipse.cucumber.ui;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class BuilderParticipant extends org.eclipse.xtext.builder.BuilderParticipant {
+	public void build(final IBuildContext context, IProgressMonitor monitor) throws CoreException {
+		super.build(context, monitor);
+	}
+}
