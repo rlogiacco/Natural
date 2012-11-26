7bc4afdaeffc4c8e8c2ede53c707e582dc44e8fe
Roberto Lo Giacco <rlogiacco@gmail.com>
2012-11-26 14:38:20 +0000

    improved performances by preventing cache cleaning on feature file change

--- org.agileware.natural.cucumber.ui/src/org/agileware/natural/cucumber/ui/BuilderParticipant.java
+++ org.agileware.natural.cucumber.ui/src/org/agileware/natural/cucumber/ui/BuilderParticipant.java
@@ -8,12 +8,13 @@ import com.google.inject.Inject;
 
 public class BuilderParticipant extends org.eclipse.xtext.builder.BuilderParticipant {
 	@Inject
-	private JavaAnnotationMatcher cache;
+	private JavaAnnotationMatcher matcher;
 	
 	public void build(final IBuildContext context, IProgressMonitor monitor) throws CoreException {
 		super.build(context, monitor);
-		if (!context.getResourceSet().getResources().isEmpty()){
-			cache.invalidateCache();
+		if (!context.getResourceSet().getResources().isEmpty()) {
+			//TODO iterate over the resources to evict only those that have changed
+//			matcher.evict(null);
 		}
 	}
 }
