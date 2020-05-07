5daded9eb31d5aa25372a79ce0120aca16118b4b
Aaron R Miller <aaron.miller@waweb.io>
2020-05-07 11:37:46 -0700

    updated CucumberValidator

--- org.agileware.natural.cucumber/src/org/agileware/natural/cucumber/validation/CucumberValidator.java
+++ org.agileware.natural.cucumber/src/org/agileware/natural/cucumber/validation/CucumberValidator.java
@@ -1,7 +1,10 @@
 package org.agileware.natural.cucumber.validation;
 
+import static org.agileware.natural.cucumber.cucumber.CucumberPackage.Literals.*;
+import static org.agileware.natural.cucumber.validation.IssueCodes.*;
+
 import org.agileware.natural.common.JavaAnnotationMatcher;
-import org.agileware.natural.cucumber.cucumber.CucumberPackage;
+import org.agileware.natural.cucumber.cucumber.Feature;
 import org.agileware.natural.cucumber.cucumber.Step;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.xtext.validation.Check;
@@ -10,29 +13,36 @@ import org.eclipse.xtext.validation.CheckType;
 import com.google.inject.Inject;
 
 public class CucumberValidator extends AbstractCucumberValidator {
-	
+
 	@Inject
 	private JavaAnnotationMatcher matcher;
 
+	@Check(CheckType.FAST)
+	public void featureHasScenarios(Feature model) {
+		if (model.getScenarios().isEmpty()) {
+			error(MissingScenarios.value(), model, FEATURE__SCENARIOS, MissingScenarios.code());
+		}
+	}
+
 	@Check(CheckType.EXPENSIVE)
-	public void checkStepMatching(Step step) {
+	public void stepsHaveMatchingDefinition(Step model) {
 		final Counter counter = new Counter();
-		String description = step.getDescription().trim();
+		String description = model.getDescription().trim();
 		matcher.findMatches(description, counter);
 		if (counter.get() == 0) {
-			warning("No definition found for `" + description + "`", CucumberPackage.Literals.STEP__DESCRIPTION);
+			warning(MissingStepDefinition.value(description), STEP__DESCRIPTION, MissingStepDefinition.code());
 		} else if (counter.get() > 1) {
-			warning("Multiple definitions found for `" + description + "`", CucumberPackage.Literals.STEP__DESCRIPTION);
+			warning(MultipleStepDefinitions.value(description), STEP__DESCRIPTION, MultipleStepDefinitions.code());
 		}
 	}
 
 	private final static class Counter implements JavaAnnotationMatcher.Command {
 		private int count = 0;
-		
+
 		public int get() {
 			return count;
 		}
-		
+
 		public void match(String annotationValue, IMethod method) {
 			count++;
 		}
---
+++ org.agileware.natural.cucumber/src/org/agileware/natural/cucumber/validation/IssueCodes.java
@@ -0,0 +1,30 @@
+package org.agileware.natural.cucumber.validation;
+
+import java.util.Formatter;
+import java.util.Locale;
+
+// TODO: load values from localized property file
+public enum IssueCodes {
+	MissingScenarios("Scenarios are undefined. At least one Scenario is needed to complete the Feature."),
+	MissingStepDefinition("No definition found for `%s`."),
+	MultipleStepDefinitions("Multiple definitions found for `%s`.");
+
+	static private final String PREFIX = "org.agileware.natural.cucumber.";
+
+	private final Formatter formatter;
+
+	private final String value;
+
+	public String code() {
+		return PREFIX + this.toString();
+	}
+
+	public String value(Object... tokens) {
+		return formatter.format(this.value, tokens).toString();
+	}
+
+	private IssueCodes(String value) {
+		this.value = value;
+		this.formatter = new Formatter(new StringBuilder(), Locale.US);
+	}
+}
