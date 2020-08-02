f2a7652a76fca110cd0742d6b030c6ba13949640
Aaron R Miller <aaron.miller@waweb.io>
2020-08-02 13:45:41 -0700

    generated core lang project bundles

---
+++ org.agileware.natural.lang.tests/src/org/agileware/natural/lang/tests/NaturalTestSuite.java
@@ -0,0 +1,8 @@
+package org.agileware.natural.lang.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ NaturalParsingTest.class })
+public class NaturalTestSuite {}
